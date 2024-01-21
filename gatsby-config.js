97ce58f9fc93239ea0db0d78b3859a87bd737b21
umeshn95 <umeshnamdev9@gmail.com>
2024-01-21 09:17:33 +0530

    confignetlifyadapter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+const adapter = require("gatsby-adapter-netlify");
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-const adapter = require("gatsby-adapter-netlify")
 module.exports = {
-  adapter: adapter({
-    excludeDatastoreFromEngineFunction: false,
-  }),
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -36,12 +25,15 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: "gatsby-adapter-netlify",
+      options: {
+        excludeDatastoreFromEngineFunction: false,
       },
     },
   ],
-}
+};
